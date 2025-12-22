ad45e7fb6cdde00b02f8bced563d3031d943113e
hayesjl77 <hayesjl77@gmail.com>
2025-12-22 08:38:16 -0500

    minor

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,14 @@
+// lib/stripe.ts (should look like this)
+'server only';
+
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-12-15.clover', // Update to the latest version (check your stripe package docs)
+if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error('Missing STRIPE_SECRET_KEY environment variable');
+}
+
+const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2025-12-15.clover', // current latest stable (Dec 2025)
 });
+
+export default stripeInstance;
