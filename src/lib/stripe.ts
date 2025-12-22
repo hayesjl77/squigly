ef0d856874f2c3ec0464f5c4a06370083978eb48
hayesjl77 <hayesjl77@gmail.com>
2025-12-22 06:10:24 -0500

    Update Stripe apiVersion to latest to fix build error

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,5 @@
-// lib/stripe.ts
-import Stripe from 'stripe'
+import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2024-06-20',
-})
+    apiVersion: '2025-12-15.clover', // Update to the latest version (check your stripe package docs)
+});
